7f1cc400a97b21b106aecda33c19f4bd17d39ef2
Avanade <Avanade@WIN-GW2C7ZXSME.corp.avanade.org>
2019-07-15 01:08:40 -0300

    graph

--- AvanadeBanking/src/app/painel/page-painel/page-painel.component.ts
+++ AvanadeBanking/src/app/painel/page-painel/page-painel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-page-painel',
@@ -8,13 +9,36 @@ import { Router } from '@angular/router';
 })
 export class PagePainelComponent implements OnInit {
   route = "";
-  
+  chart:any = [];
+
   constructor(private routes: Router) { }
-
-
-
+  
+  
   ngOnInit() {
     this.route = this.routes.url;
+
+    this.chart = new Chart('canvas', {
+      type:'line',
+      data: {
+        labels: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul'],
+        datasets: [
+          {
+            label: "Gastos",
+            data: [1,3,5,10,56,65,35,53,53].reverse(),
+            backgroundColor: "red",
+            borderColor: "red",
+            fill: true,
+          },
+          {
+            label: "Investimentos",
+            data: [1,3,5,10,56,65,35,543,543],
+            backgroundColor: "blue",
+            borderColor: "blue",
+            fill: false,
+          }
+        ]
+      }
+    })
   }
 
 }
